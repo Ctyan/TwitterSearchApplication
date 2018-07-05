f3ccb8f10ee218632a13ef38b1114ab9d027db26
Ctyan <im12800a@gmail.com>
2018-07-05 21:32:24 +0900

    除外するユーザーのクラスを用意。 持ってくるツイートの表示を改善。

--- src/core/GetTweet.java
+++ src/core/GetTweet.java
@@ -11,47 +11,59 @@ import twitter4j.TwitterFactory;
 
 public class GetTweet {
 
-	Twitter twitter;
-	Query query;
+	private Twitter twitter;
+	private Query query;
+	private RemoveUser removeUser;
 
 	private int GET_TWEET_SIZE = 100;
+	private String[] KEYWORDS = {"運転", "影響", "再開", "復旧", "復活", "遅延", "遅", "点検", "見合わせ", "停", "止", "死", "人身", "事故"};
 
 	public GetTweet(){
 		twitter = new TwitterFactory().getInstance();
 		query = new Query();
 		query.setLang("ja");
 		query.setCount(GET_TWEET_SIZE);
-		query.setSince("2018-06-10");
-		query.setUntil("2018-06-16");
+		query.setSince("2018-07-01");
+		query.setUntil("2018-07-06");
+		removeUser = new RemoveUser();
 	}
 
 	public void searchTweet() throws TwitterException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		String message = new String();
+
 		// 検索語指定
-		query.setQuery("遅延");
+		query.setQuery("日比谷線 -rt -bot");
 
 		// 検索実行
 		QueryResult result = twitter.search(query);
 
 		// 検索結果を見てみる
-		int page = 0;
-		while(true) {
-			int tweetCount =0;
-			System.out.println("ページ数 : " + ++page);
-			System.out.println("ヒット数 : " + result.getTweets().size());
+		int tweetCount =0;
 
-			for (Status tweet : result.getTweets()) {
-				System.out.println("T "+String.format("%3s", Integer.toString(++tweetCount+(page-1)*GET_TWEET_SIZE)));
-				System.out.println("U "+tweet.getUser().getScreenName());
-				//System.out.println("C "+tweet.getText());
-				System.out.println("D "+new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(tweet.getCreatedAt())+" ["+tweet.getCreatedAt().getTime()+"]");
-				System.out.println();
-			}
+		for (Status tweet : result.getTweets()) {
+			if(removeUser.isEnterUser(tweet.getUser().getScreenName())) continue;
 
-			if (result.hasNext()) {
-				query = result.nextQuery();
-			} else {
-				break;
+			System.out.println("No"+String.format("%3s", Integer.toString(++tweetCount))+" daytime: "+sdf.format(tweet.getCreatedAt()));
+			message = tweet.getText().replaceAll("[\r\n]", "<br>");
+			System.out.println(containKeywordsCheck(message));
+			System.out.println(message);
+			System.out.println();
+		}
+	}
+
+	private String containKeywordsCheck(String mes){
+		String ret = new String();
+
+		for(String key : KEYWORDS){
+			if(mes.contains(key)){
+				ret += key+" * ";
+			}else{
+				ret += key+"   ";
 			}
 		}
+
+		return ret;
 	}
 }
---
+++ src/core/RemoveUser.java
@@ -0,0 +1,13 @@
+package core;
+
+public class RemoveUser {
+
+	public RemoveUser() {
+		//
+	}
+
+	public boolean isEnterUser(String str){
+		return false;
+		//
+	}
+}
